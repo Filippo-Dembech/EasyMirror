c308367b29c5b36b7f6a2a433c41c62aa49024b5
Filippo Dembech <Filippo.dembech@gmail.com>
2024-05-06 09:25:56 +0200

    add: Delimiters.of() and Delimiters.countOperation()

--- lib/src/method_analyzer/string_extractor/delimiters.dart
+++ lib/src/method_analyzer/string_extractor/delimiters.dart
@@ -9,8 +9,26 @@ enum Delimiters {
 
   const Delimiters(this.opening, this.closing);
 
-  static bool isOpeningDelimiter(String string) => Delimiters.values.any((delimiters) => delimiters.opening == string);
-  static bool isClosingDelimiter(String string) => Delimiters.values.any((delimiters) => delimiters.closing == string);
+  static bool isOpeningDelimiter(String string) =>
+      Delimiters.values.any((delimiters) => delimiters.opening == string);
+
+  static bool isClosingDelimiter(String string) =>
+      Delimiters.values.any((delimiters) => delimiters.closing == string);
+
+  static Delimiters of(String string) =>
+      Delimiters.values.firstWhere((delimiters) => delimiters.contains(string));
+
+  static countOperation(
+      Delimiters delimiters,
+      Function(
+        Delimiters delimiters,
+        int openingCount,
+        int closingCount,
+      ) f) {
+    int openingCount = 1;
+    int closingCount = 0;
+    f(delimiters, openingCount, closingCount);
+  }
 
   bool contains(String string) => opening == string || closing == string;
 
