8f82931313561d200da39fb9f3d6483ca11fd992
Filippo Dembech <Filippo.dembech@gmail.com>
2024-05-08 13:04:53 +0200

    cleanup: UnmatchingDelimitersException no longer needed

--- lib/src/method_analyzer/string_extractor/string_extractor.dart
+++ lib/src/method_analyzer/string_extractor/string_extractor.dart
@@ -121,22 +121,3 @@ final class ViseExtraction extends Extraction {
       return [];
   }
 }
-
-/*
-class UnmatchingDelimitersException implements Exception {
-  Delimiters _delimiters;
-  int _openingDelimitersCount = 0;
-  int _closingDelimitersCount = 0;
-
-  UnmatchingDelimitersException(
-    this._delimiters,
-    this._openingDelimitersCount,
-    this._closingDelimitersCount,
-  );
-
-  @override
-  String toString() =>
-      "NonMatchingDelimitersException: non matching delimiters '${_delimiters.opening}' -> count: $_openingDelimitersCount, '${_delimiters.closing}' -> count: $_closingDelimitersCount";
-}
-
-*/
