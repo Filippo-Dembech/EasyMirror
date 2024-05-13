e55e3997ffb111de997b0c27134749d9ae9b25c7
Filippo Dembech <Filippo.dembech@gmail.com>
2024-05-13 12:32:22 +0200

    docs: Parameter

--- lib/src/method_analyzer/parameter.dart
+++ lib/src/method_analyzer/parameter.dart
@@ -1,16 +1,39 @@
 import 'parameter_type.dart';
 
 class Parameter {
+  /// The [name] of this parameter.
   final String name;
+
+  /// The [dataType] of this parameter.
   final String dataType;
+
+  /// Whether this parameter is a positional parameter.
   final bool isPositional;
+
+  /// Whether this parameter is an optional positional parameter.
   final bool isOptionalPositional;
+
+  /// Whether this parameter is a named parameter.
   final bool isNamed;
+
+  /// Whether this parameter is optional.
   final bool isOptional;
+
+  /// Whether this parameter is required.
   final bool isRequired;
+
+  /// Whether this parameter has some default value.
   final bool hasDefaultValue;
+
+  /// Whether this parameter is nullable.
   final bool isNullable;
+
+  /// The [ParameterType] of this parameter.
   final ParameterType type;
+
+  /// The default value of this parameter, if any.
+  /// If no default parameter is present, `null` is
+  /// returned.
   final String? defaultValue;
 
   const Parameter({
